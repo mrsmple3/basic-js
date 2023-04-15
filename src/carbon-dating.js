e5408ca5556d524063ec0bf788d8e35be6f5baba
Muhammadrajab <muhammadr0jabxujaev@gmail.com>
2023-04-15 16:50:09 +0500

    init: second task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,22 +6,28 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let age = false;
+  if (typeof sampleActivity !== "string") {
+    return age;
+  }
+  age = Math.ceil(Math.log(MODERN_ACTIVITY / parseInt(sampleActivity)) / k);
+  return age;
 }
-
+let number = "2";
+console.log(dateSample(number));
 module.exports = {
-  dateSample
+  dateSample,
 };
